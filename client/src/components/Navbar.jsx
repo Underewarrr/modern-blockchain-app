e0e1246f6795780ccfe72433e63ccba15dfe2f27
underewarrr <underewar@gmail.com>
2022-04-20 22:23:09 -0300

    Dev: icons from react included, Navbar started

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,18 @@
+import {HiMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
 const Navbar = () => {
  return (
-     <h1>Navbar</h1>
+     <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+         <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+          <img src={logo} alt='logo' className='w-32 cursor-pointer' />
+         </div>
+         <ul className='text-white md:flex hiddent list-none flex-row justify-between items-center flex-init'>
+
+         </ul>
+     </nav>
  );
 }
 
