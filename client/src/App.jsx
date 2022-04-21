6c101128e296bfc1a420e8caf1b5a5951c1a8551
underewarrr <underewar@gmail.com>
2022-04-20 22:04:16 -0300

    Dev: index.css Included Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,14 @@ import { Navbar, Welcome, Footer, Services, Transactions} from './components';
 const App = () => {
 
   return (
-    <div className="min-h-screen"> // min-h-screen is a global CSS class
+    <div className="min-h-screen">
        <div className='gradient-bg-welcome'>
-        <Navbar />
-        <Welcome />
+          <Navbar />
+          <Welcome />
+       </div>
         <Services />
         <Transactions />
         <Footer />
-       </div>
     </div>
   )
 }
