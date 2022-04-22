29caf51573dfed29a77297825eb32ad280413ad4
underewarrr <underewar@gmail.com>
2022-04-22 02:23:45 -0300

    TimeStamp and message included to Last Transaction history

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -7,7 +7,7 @@ import dummyData from '../utils/dummyData';
 
 import { shortenAddress } from '../utils/shortenAddress';
 
-const TransactionCard = ({addressTo, addressFrom, timeStamp, message, keyword, amount, url}) => {
+const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
     return (
         <div className="bg-[#181918] m-4 flex flex-1
         2xl:min-w-[450px]
@@ -18,7 +18,7 @@ const TransactionCard = ({addressTo, addressFrom, timeStamp, message, keyword, a
         flex-col p-3 rounded-md hover:shadow-2xl"
       >
         <div className="flex flex-col items-center w-full mt-3">
-          <div className="display-flex justify-start w-full mb-6 p-2">
+          <div className="w-full mb-6 p-2">
             <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
               <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
             </a>
@@ -26,6 +26,19 @@ const TransactionCard = ({addressTo, addressFrom, timeStamp, message, keyword, a
               <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
             </a>
             <p className='text-white text-base'>Amount: {amount} ETH</p>
+            {message && (
+                <>
+                <br />
+                <p className='text-white text-base'>Message: {message}</p>
+                </>
+            )}
+
+                <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl'>
+                    <p className='text-[#37c7da] font-bold'> 
+                    {timestamp}
+                    </p>
+                </div>
+
          </div>
 
         </div>
